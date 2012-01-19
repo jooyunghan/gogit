b3cbf0580d404a0da2e66e60ebe3b780282e2ff1
Jooyung Han <jooyung.han@gmail.com>
2012-01-19 15:37:49 +0900

    list up branches from git dir

--- main.go
+++ main.go
@@ -1,8 +1,58 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"path"
+)
 
-// list branches
+
+func gitdir() string {
+	pwd, err := os.Getwd()
+	if err != nil {
+		return ""
+	}
+
+	for {
+		candidate := path.Join(pwd, ".git")
+		fileInfo, err := os.Stat(candidate)
+		if err == nil && fileInfo.IsDir() {
+			return candidate
+		}
+		if pwd == "/" {
+			break
+		}
+		pwd = path.Dir(pwd) 
+	}
+	panic("no git dir")
+}
+
+func gitBranches(gitdir string) []string {
+	refsHeads := path.Join(gitdir, "refs/heads")
+	if _, err := os.Open(path.Join(refsHeads)); err != nil {
+		panic("Open git dir failed")
+	}
+	return dirFiles(refsHeads)
+}
+
+func dirFiles(baseDir string) (files []string) {
+	base, _ := os.Open(baseDir)
+	fi, _ := base.Readdir(-1)
+	for _, f := range fi {
+		if f.IsDir() {
+			names := dirFiles(path.Join(baseDir, f.Name()))
+			for _, name := range names {
+				files = append(files, path.Join(f.Name(), name))
+			}
+		} else {
+			files = append(files, f.Name())
+		}
+	}
+	return
+}
+
 func main() {
-	fmt.Println("Hello World")
+	dir := gitdir()
+	branches := gitBranches(dir)
+	fmt.Println(branches)
 }
